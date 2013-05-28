2616ef198b611c8386a83be58ff224b8d71a452e
aalmiray <aalmiray@gmail.com>
2013-05-28 11:45:30 +0200

    Added New, Open, Save, SaveAs actions Document conversion is now triggered using meta+K or selecting a menu action View will display preview and generated source in tabs

--- griffon-app/controllers/doctorpad/DoctorpadController.groovy
+++ griffon-app/controllers/doctorpad/DoctorpadController.groovy
@@ -1,5 +1,7 @@
 package doctorpad
 
+import griffon.transform.Threading
+import javafx.stage.FileChooser
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.Attributes
 import org.asciidoctor.OptionsBuilder
@@ -17,17 +19,60 @@ class DoctorpadController {
     private Asciidoctor instance = Asciidoctor.Factory.create()
     private static Attributes attr
     private Map options = OptionsBuilder.options()
-            .compact(false)
-            .headerFooter(true)
-            .safe(SafeMode.UNSAFE)
-            .backend("html5")
-            .attributes(attr)
-            .asMap()
+        .compact(false)
+        .headerFooter(true)
+        .safe(SafeMode.UNSAFE)
+        .backend("html5")
+        .attributes(attr)
+        .asMap()
 
-    def convertAction = { evt = null ->
-        def output = instance.render(model.input, options)
-        println output
+    def convertAction = {
+        updatePreview()
+    }
+
+    def newAction = {
+        model.documentFileName = null
+        model.input = ''
+    }
+
+    @Threading(Threading.Policy.SKIP)
+    def openAction = {
+        FileChooser fileChooser = new FileChooser()
+        fileChooser.title = app.getMessage('application.dialog.Open.title', 'Open document')
+        String documentFileName = fileChooser.showOpenDialog(view.mainWindow)?.absolutePath
+        if (documentFileName) {
+            execOutsideUI {
+                model.documentFileName = documentFileName
+                String documentText = new File(documentFileName).text
+                model.input = documentText
+                updatePreview()
+            }
+        }
+    }
+
+    def saveAction = {
+        if (model.documentFileName) {
+            new File(model.documentFileName).write(model.input)
+        } else {
+            saveAsAction()
+        }
+    }
+
+    @Threading(Threading.Policy.SKIP)
+    def saveAsAction = {
+        FileChooser fileChooser = new FileChooser()
+        fileChooser.title = app.getMessage('application.dialog.Save.title', 'Save document')
+        String documentFileName = fileChooser.showSaveDialog(view.mainWindow)?.absolutePath
+        if (documentFileName) {
+            model.documentFileName = documentFileName
+            new File(model.documentFileName).write(model.input)
+        }
+    }
+
+    private void updatePreview() {
+        String output = instance.render(model.input, options)
         execInsideUIAsync {
+            model.output = output
             view.webview.engine.loadContent(output)
         }
     }
